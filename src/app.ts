3847105ab43c2bbb2fe39aa7703c79bc9b668847
lucas <lucmaieski@gmail.com>
2022-04-28 16:31:40 -0300

    Add cors settings

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,15 @@ import { userRouter } from './routes/userRouter';
 
 const app: Express = express();
 
+const corsOptions = {
+    origin: '*',
+    methods: 'GET, PUT, POST, DELETE',
+    optionsSuccessStatus: 200,
+};
+
 app.use(express.json());
 
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use('/user', userRouter);
 app.use('/tweet', tweetRouter);
